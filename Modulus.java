7cab4ce5ca9154185a70e421f4af908e49ba50e0
22P31A0438 <119720806+22P31A0438@users.noreply.github.com>
2023-05-01 22:16:04 +0530

    import java.util.*; public class Sample {     public static void main(String args[])     {         Scanner mr=new Scanner(System.in);         int a,b,c;         a=mr.nextInt();         b=mr.nextInt();         c=a%b;         System.out.println(c);     } }

---
+++ Modulus.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner mr=new Scanner(System.in);
+        int a,b,c;
+        a=mr.nextInt();
+        b=mr.nextInt();
+        c=a%b;
+        System.out.println(c);
+    }
+}
