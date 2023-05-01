de000a9b86cb0516417be7b75a06b0713fb15694
22P31A0438 <119720806+22P31A0438@users.noreply.github.com>
2023-05-01 22:11:01 +0530

    import java.util.*; public class Sample {     public static void main(String args[])     {         Scanner mr=new Scanner(System.in);         int a;      double r;         a=mr.nextInt();         r=(3.14)*a*a;         System.out.format("%.2f",r);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner mr=new Scanner(System.in);
+        int a;
+     double r;
+        a=mr.nextInt();
+        r=(3.14)*a*a;
+        System.out.format("%.2f",r);
+    }
+}
