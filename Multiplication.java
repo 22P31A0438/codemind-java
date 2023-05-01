6fcf0f00eca033f50086e1dae892467f7afde497
22P31A0438 <119720806+22P31A0438@users.noreply.github.com>
2023-05-01 21:52:03 +0530

    import java.util.*; public class Sample {     public static void main(String args[])     {         Scanner mr=new Scanner(System.in);         int m,r,n;         m=mr.nextInt();         r=mr.nextInt();         n=m*r;         System.out.println(n);     } }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner mr=new Scanner(System.in);
+        int m,r,n;
+        m=mr.nextInt();
+        r=mr.nextInt();
+        n=m*r;
+        System.out.println(n);
+    }
+}
