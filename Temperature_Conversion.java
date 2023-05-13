667372efb23caa2165c31de504c9f75217c8b180
22P31A0438 <119720806+22P31A0438@users.noreply.github.com>
2023-05-13 21:54:08 +0530

    import java.util.*; public class Sample {     public static void main(String args[])     {         Scanner mr=new Scanner(System.in);         int c;         c=mr.nextInt();         float f;         f=32+(c*(float)9/5);         System.out.format("%.2f",f);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner mr=new Scanner(System.in);
+        int c;
+        c=mr.nextInt();
+        float f;
+        f=32+(c*(float)9/5);
+        System.out.format("%.2f",f);
+    }
+}
