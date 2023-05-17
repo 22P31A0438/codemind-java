3ae11ce1fd6b044bdc287107cde18579d8810edd
22P31A0438 <119720806+22P31A0438@users.noreply.github.com>
2023-05-17 09:06:05 +0530

    import java.util.*; public class Sample {     public static void main(String args[])     {         Scanner mr=new Scanner(System.in);         int t;         float x1,y1,x2,y2;         t=mr.nextInt();         while(t-->0)         {         x1=mr.nextFloat();         x2=mr.nextFloat();         y1=mr.nextFloat();         y2=mr.nextFloat();         if(y1/x1<y2/x2)         System.out.println("-1");         else if(y1/x1>y2/x2)         System.out.println("1");         else         System.out.println("0");         }     } }

---
+++ Car_Choice.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner mr=new Scanner(System.in);
+        int t;
+        float x1,y1,x2,y2;
+        t=mr.nextInt();
+        while(t-->0)
+        {
+        x1=mr.nextFloat();
+        x2=mr.nextFloat();
+        y1=mr.nextFloat();
+        y2=mr.nextFloat();
+        if(y1/x1<y2/x2)
+        System.out.println("-1");
+        else if(y1/x1>y2/x2)
+        System.out.println("1");
+        else 
+        System.out.println("0");
+        }
+    }
+}
