6be00d4e9710cb06a15b3465ac3844b3cc173a58
22P31A0438 <119720806+22P31A0438@users.noreply.github.com>
2023-05-17 10:35:15 +0530

    import java.util.*; public class Sample {     public static void main(String args[])     {         Scanner mr=new Scanner(System.in);         int t;         Float x1,x2,y1,y2;         t=mr.nextInt();         while(t-->0)         {             x1=mr.nextFloat();             x2=mr.nextFloat();             y1=mr.nextFloat();             y2=mr.nextFloat();             if(y1/x1<y2/x2)             System.out.println("-1");             else if(y1/x1>y2/x2)             System.out.println("1");             else             System.out.println("0");         }         mr.close();     } }

--- Car_Choice.java
+++ Car_Choice.java
@@ -5,20 +5,21 @@ public class Sample
     {
         Scanner mr=new Scanner(System.in);
         int t;
-        float x1,y1,x2,y2;
+        Float x1,x2,y1,y2;
         t=mr.nextInt();
         while(t-->0)
         {
-        x1=mr.nextFloat();
-        x2=mr.nextFloat();
-        y1=mr.nextFloat();
-        y2=mr.nextFloat();
-        if(y1/x1<y2/x2)
-        System.out.println("-1");
-        else if(y1/x1>y2/x2)
-        System.out.println("1");
-        else 
-        System.out.println("0");
+            x1=mr.nextFloat();
+            x2=mr.nextFloat();
+            y1=mr.nextFloat();
+            y2=mr.nextFloat();
+            if(y1/x1<y2/x2)
+            System.out.println("-1");
+            else if(y1/x1>y2/x2)
+            System.out.println("1");
+            else 
+            System.out.println("0");
         }
+        mr.close();
     }
 }
